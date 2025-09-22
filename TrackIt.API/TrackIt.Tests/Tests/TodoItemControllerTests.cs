03e7ec12b9c5f5905fe9c06c278b42cc36e91852
Kiss Tomi <kisstomi7.kt@gmail.com>
2025-09-22 23:25:39 +0300

    Created unit tests for controller endpoints

---
+++ TrackIt.API/TrackIt.Tests/Tests/TodoItemControllerTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TrackIt.API.Controllers;
+using TrackIt.Application.DTOs;
+using TrackIt.Application.Services;
+
+namespace TrackIt.Tests.Tests
+{
+    [TestClass]
+    public class TodoItemControllerTests
+    {
+        private Mock<ITodoItemService> _mockTodoItemService;
+        private TodoItemController _controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockTodoItemService = new Mock<ITodoItemService>();
+            _controller = new TodoItemController(_mockTodoItemService.Object);
+        }
+
+        [TestMethod]
+        public async Task GetPendingTodoItems_ReturnsNoContent_WhenNoneFound()
+        {
+            // ARRANGE
+            _mockTodoItemService.Setup(s => s.GetPendingTodoItemsAsync()).ReturnsAsync([]);
+
+            // ACT
+            var result = await _controller.GetPendingTodoItems();
+
+            // ASSERT
+            Assert.IsInstanceOfType(result.Result, typeof(NoContentResult));
+        }
+
+        [TestMethod]
+        public async Task GetPendingTodoItems_ReturnsOk_WithItems()
+        {
+            // ARRANGE
+            var items = new List<TodoItemDto> { new() { Name = "Test" } };
+            _mockTodoItemService.Setup(s => s.GetPendingTodoItemsAsync()).ReturnsAsync(items);
+
+            // ACT
+            var result = await _controller.GetPendingTodoItems();
+
+            // ASSERT
+            var okResult = result.Result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(items, okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task CreateTodoItem_ReturnsOk()
+        {
+            // ARRANGE
+            var dto = new TodoItemCreateDto() { Name = "Test" };
+            _mockTodoItemService.Setup(s => s.CreateTodoItemAsync(dto)).Returns(Task.CompletedTask);
+
+            // ACT
+            var result = await _controller.CreateTodoItem(dto);
+
+            // ASSERT
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+            _mockTodoItemService.Verify(s => s.CreateTodoItemAsync(dto), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task MarkAsDone_ReturnsNotFound_WhenItemDoesNotExist()
+        {
+            // ARRANGE
+            int id = 1;
+            _mockTodoItemService.Setup(s => s.MarkAsDone(id)).ReturnsAsync(false);
+
+            // ACT
+            var result = await _controller.MarkAsDone(id);
+
+            // ASSERT
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public async Task MarkAsDone_ReturnsOk_WhenTodoItemIsMarkedDone()
+        {
+            // ARRANGE
+            int id = 1;
+            _mockTodoItemService.Setup(s => s.MarkAsDone(id)).ReturnsAsync(true);
+
+            // ACT
+            var result = await _controller.MarkAsDone(id);
+
+            // ASSERT
+            Assert.IsInstanceOfType(result, typeof(NoContentResult));
+        }
+    }
+}
