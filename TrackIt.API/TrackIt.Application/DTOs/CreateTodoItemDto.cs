127ae2478c700da55e452e73fa6e5e13114e076c
Kiss Tomi <kisstomi7.kt@gmail.com>
2025-09-23 22:25:47 +0300

    Added data annotations for my dto

--- TrackIt.API/TrackIt.Application/DTOs/CreateTodoItemDto.cs
+++ TrackIt.API/TrackIt.Application/DTOs/CreateTodoItemDto.cs
@@ -1,11 +1,15 @@
-﻿using TrackIt.Domain.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using TrackIt.Domain.Enums;
 
 namespace TrackIt.Application.DTOs
 {
     public class CreateTodoItemDto
     {
+        [Required(ErrorMessage = "Name is required.")]
+        [MaxLength(64, ErrorMessage = "Name cannot exceed 64 characters.")]
         public required string Name { get; set; }
 
+        [MaxLength(256, ErrorMessage = "Description cannot exceed 256 characters.")]
         public string? Description { get; set; }
 
         public PriorityEnum Priority { get; set; }
